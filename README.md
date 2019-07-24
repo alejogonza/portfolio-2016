6cf53c00ac411e0b6b4e72f66543c64cc477d31e
alejogonza <alejo.1996.2001@gmail.com>
2019-07-24 12:40:08 -0500

    [update] update my portfolio page

---
+++ README.md
@@ -0,0 +1,11 @@
+## Portfolio 2016
+
+<p align="center">
+  <img width="250" height="250" src="https://www.apereo.org/sites/default/files/styles/slider_large/public/OA2016.jpg?itok=Q5eUUF0q">
+</p>
+
+## Hi this is my portfolio :)
+this is my portfolio page when I did the tests i hope you like it :)
+
+
+look the page here: [My wonderful portfolio page :)]()
---
+++ js/background.js
@@ -0,0 +1,13 @@
+ $(function() {
+$(".glitch-img").mgGlitch({
+                    destroy: false,
+                    glitch: true,
+                    scale: true,
+                    blend: true,
+                    blendModeType: 'hue',
+                    glitch1TimeMin: 450,
+                    glitch1TimeMax: 450,
+                    glitch2TimeMin: 425,
+                    glitch2TimeMax: 450,
+               });
+          });
